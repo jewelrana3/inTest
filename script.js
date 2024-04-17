da56ff1831e1bd1f5880a5e9cd95b4b82e363d52
juyelbai <juyelhaque29@gmail.com>
2024-04-18 00:22:27 +0600

    this is first commit nav and header side complete

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleItemList() {
+  var itemList = document.getElementById("itemList");
+  if (itemList.style.display === "none") {
+    itemList.style.display = "block";
+  } else {
+    itemList.style.display = "none";
+  }
+}
