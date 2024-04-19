5e64f6ce8fb1d8ca22bd24546722a489e14dcb84
juyelbai <juyelhaque29@gmail.com>
2024-04-19 07:31:27 +0600

    navber section end and sideber start

--- script.js
+++ script.js
@@ -1,8 +1,73 @@
+// notification js
+
 function toggleItemList() {
-  var itemList = document.getElementById("itemList");
+  let itemList = document.getElementById("itemList");
   if (itemList.style.display === "none") {
     itemList.style.display = "block";
   } else {
     itemList.style.display = "none";
   }
+}
+
+// imgList section
+function imgList() {
+  let itemList = document.getElementById("imgListId");
+  if (itemList.style.display === "none") {
+    itemList.style.display = "block";
+  } else {
+    itemList.style.display = "none";
+  }
+}
+
+// Get the modal
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("openModalButton");
+let body = document.body;
+
+// Get the buttons that close the modal
+let closeSpan = document.getElementById("closeBtn");
+let closeModalBtn = document.getElementById("closeModalBtn");
+
+// When the user clicks on the buttons, close the modal
+closeSpan.onclick = closeModal;
+closeModalBtn.onclick = closeModal;
+modal.style.display = "none";
+// When the user clicks on the button, open the modal
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    closeModal();
+  }
+};
+
+// Function to close the modal
+function closeModal() {
+  modal.style.display = "none";
+}
+
+// Get the moon icon element
+const iconDark = document.getElementById("icon-dark");
+
+// Toggle between light and dark mode when clicking the moon icon
+iconDark.addEventListener("click", function () {
+  // Toggle the dark-mode class on the body
+  document.body.classList.toggle("dark-mode");
+});
+
+// side ber menu
+function w3_open() {
+  document.getElementById("main").style.marginLeft = "25%";
+  document.getElementById("mySidebar").style.width = "25%";
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("openNav").style.display = "none";
+}
+function w3_close() {
+  document.getElementById("main").style.marginLeft = "0%";
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("openNav").style.display = "inline-block";
 }
